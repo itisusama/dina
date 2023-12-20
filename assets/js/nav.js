854b6408c1ee6b98d58cf3c3fb6e120ce7b5df14
Usama Hassan <ahmarkhan1233456@gmail.com>
2023-12-20 09:57:04 +0500

    footer update

--- assets/js/nav.js
+++ assets/js/nav.js
@@ -23,40 +23,40 @@ closeNav.addEventListener('click', () => {
 // Collection
 // init Isotope
 var $grid = $('.collection-list').isotope({
-    // options
+  // options
+});
+// filter items on button click
+$('.filter-button-group').on( 'click', 'button', function() {
+  var filterValue = $(this).attr('data-filter');
+  resetFilterBtns();
+  $(this).addClass('active-filter-btn');
+  $grid.isotope({ filter: filterValue });
+});
+
+var filterBtns = $('.filter-button-group').find('button');
+function resetFilterBtns(){
+  filterBtns.each(function(){
+    $(this).removeClass('active-filter-btn');
   });
-  // filter items on button click
-  $('.filter-button-group').on( 'click', 'button', function() {
-    var filterValue = $(this).attr('data-filter');
-    resetFilterBtns();
-    $(this).addClass('active-filter-btn');
-    $grid.isotope({ filter: filterValue });
-  });
-  
-  var filterBtns = $('.filter-button-group').find('button');
-  function resetFilterBtns(){
-    filterBtns.each(function(){
-      $(this).removeClass('active-filter-btn');
-    });
-  }
-  
-  // Product
-  const imgs = document.querySelectorAll('.img-select a');
+}
+
+// Product
+const imgs = document.querySelectorAll('.img-select a');
 const imgBtns = [...imgs];
 let imgId = 1;
 
 imgBtns.forEach((imgItem) => {
-    imgItem.addEventListener('click', (event) => {
-        event.preventDefault();
-        imgId = imgItem.dataset.id;
-        slideImage();
-    });
+  imgItem.addEventListener('click', (event) => {
+      event.preventDefault();
+      imgId = imgItem.dataset.id;
+      slideImage();
+  });
 });
 
 function slideImage(){
-    const displayWidth = document.querySelector('.img-showcase img:first-child').clientWidth;
+  const displayWidth = document.querySelector('.img-showcase img:first-child').clientWidth;
 
-    document.querySelector('.img-showcase').style.transform = `translateX(${- (imgId - 1) * displayWidth}px)`;
+  document.querySelector('.img-showcase').style.transform = `translateX(${- (imgId - 1) * displayWidth}px)`;
 }
 
 window.addEventListener('resize', slideImage);
